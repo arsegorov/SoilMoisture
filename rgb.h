596e6d457cd6f3f027ee148f336a1c2c669b6b24
Arseny Egorov <arsegorov@gmail.com>
2022-11-02 23:08:22 -0700

    Update  module implementation and docstrings

--- rgb.h
+++ rgb.h
@@ -1,18 +1,20 @@
 #ifndef __RGB_H__
 #define __RGB_H__
 
-const uint8_t RED_PIN = 26;
-const uint8_t GREEN_PIN = 25;
-const uint8_t BLUE_PIN = 27;
+const uint8_t RED_PIN = 26,
+              GREEN_PIN = 25,
+              BLUE_PIN = 27;
 
-const uint8_t RED = 4;
-const uint8_t GREEN = 2;
-const uint8_t BLUE = 1;
-const uint8_t BLACK = 0;
-const uint8_t YELLOW = RED | GREEN;
-const uint8_t MAGENTA = RED | BLUE, PURPLE = MAGENTA;
-const uint8_t CYAN = GREEN | BLUE, TEAL = CYAN;
-const uint8_t WHITE = RED | GREEN | BLUE;
+const uint8_t RED = 4,
+              GREEN = 2,
+              BLUE = 1,
+              BLACK = 0,
+              YELLOW = RED | GREEN,
+              MAGENTA = RED | BLUE,
+              PURPLE = MAGENTA,
+              CYAN = GREEN | BLUE,
+              TEAL = CYAN,
+              WHITE = RED | GREEN | BLUE;
 
 const uint8_t BRIGHT = 255,
               DIM = 191,
@@ -27,11 +29,20 @@ const uint8_t ZERO = 0UL,
               ONE = 1000UL;
 
 void rgb_setup();
-// void color_on(uint8_t pin, uint8_t intensity);
-// void color_off(uint8_t pin);
 void rgb(uint8_t color, uint8_t intensity);
-void blink(uint8_t color, uint8_t intensity, unsigned long ms_on, int repeat = 1, unsigned long ms_off = INT32_MAX);
+void blink(
+  uint8_t color,
+  uint8_t intensity,
+  unsigned long ms_on,
+  int repeat = 1,
+  unsigned long ms_off = INT32_MAX
+);
 void rgb_16k(int color);
-void blink_16k(int color, unsigned long ms_on, int repeat = 1, unsigned long ms_off = INT32_MAX);
+void blink_16k(
+  int color,
+  unsigned long ms_on,
+  int repeat = 1,
+  unsigned long ms_off = INT32_MAX
+);
 
 #endif
--- rgb.ino
+++ rgb.ino
@@ -4,6 +4,10 @@
 #include "rgb.h"
 
 
+/**
+ * Initializes the RGB LED control pins
+ *  and blinks each of the 3 colors.
+*/
 void rgb_setup() {
   Serial.println("Setting up RGB LED...");
 
@@ -23,40 +27,27 @@ void rgb_setup() {
 }
 
 
-void color_on(uint8_t pin, uint8_t intensity) {
+/**
+ * Sets an RGB pin signal intensity.
+ * 
+ * @param pin one of the RGB LED's control pins: RED_PIN, GREEN_PIN, or BLUE_PIN
+ * @param intensity desired intensity, 0..255
+*/
+void set_intensity(uint8_t pin, uint8_t intensity) {
   WiFiDrv::analogWrite(pin, intensity);
 }
 
 
-void color_off(uint8_t pin) {
-  color_on(pin, (uint8_t)0);
-}
-
-
 /**
- * Turns the RGB LED on with the 3-bit color and 8-bit intensity.
+ * Turns on the RGB LED with the 3-bit color and 8-bit intensity.
  * 
  * @param color 3-bit color, the high 5 bits are ignored
  * @param intensity 8-bit intensity
 */
 void rgb(uint8_t color, uint8_t intensity) {
-  if (color & RED) {
-    color_on(RED_PIN, intensity);
-  } else {
-    color_off(RED_PIN);
-  }
-
-  if (color & BLUE) {
-    color_on(BLUE_PIN, intensity);
-  } else {
-    color_off(BLUE_PIN);
-  }
-
-  if (color & GREEN) {
-    color_on(GREEN_PIN, intensity);
-  } else {
-    color_off(GREEN_PIN);
-  }
+  set_intensity(RED_PIN, color & RED ? intensity : (uint8_t)0);
+  set_intensity(GREEN_PIN, color & GREEN ? intensity : (uint8_t)0);
+  set_intensity(BLUE_PIN, color & BLUE ? intensity : (uint8_t)0);
 }
 
 
@@ -82,17 +73,34 @@ void blink(uint8_t color, uint8_t intensity, unsigned long ms_on, int repeat = 1
 }
 
 
+/**
+ * Turns on the RGB LED with the given 16-bit color.
+ * 
+ * @param color 16-bit color;
+ *  the highest 4 bits are overall intensity adjustment,
+ *  the next 4 are red color intensity, the next 4 - green, the lowest 4 - blue.
+*/
 void rgb_16k(int color) {
-  uint8_t red_intensity = (color >> 8) & 0b1111,
-          green_intensity = (color >> 4) & 0b1111,
+  uint8_t red_intensity = (color>>8) & 0b1111,
+          green_intensity = (color>>4) & 0b1111,
           blue_intensity = color & 0b1111,
-          alpha = color >> 12;
-  color_on(RED_PIN, red_intensity * alpha);
-  color_on(GREEN_PIN, green_intensity * alpha);
-  color_on(BLUE_PIN, blue_intensity * alpha);
+          alpha = (color>>8) & 0b11110000;
+  set_intensity(RED_PIN, alpha + red_intensity);
+  set_intensity(GREEN_PIN, alpha + green_intensity);
+  set_intensity(BLUE_PIN, alpha + blue_intensity);
 }
 
 
+/**
+ * Blinks the RGB LED with the given 16-bit color, time on, time off, and number of repeats.
+ * 
+ * @param color 16-bit color;
+ *  the highest 4 bits are overall intensity adjustment,
+ *  the next 4 are red color intensity, the next 4 - green, the lowest 4 - blue.
+ * @param ms_on the time in ms the LED should be on
+ * @param repeat how many times to blink
+ * @param ms_off the time in ms the LED should be off
+*/
 void blink_16k(int color, unsigned long ms_on, int repeat = 1, unsigned long ms_off = INT32_MAX) {
   if (ms_off == INT32_MAX) {
     ms_off = ms_on;
