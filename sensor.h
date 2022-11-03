59cf0b515b9bb3179845892762b66afdaa9eb817
Arseny Egorov <arsegorov@gmail.com>
2022-11-03 01:23:19 -0700

    Added `SATURATED_READING` value

--- sensor.h
+++ sensor.h
@@ -3,8 +3,9 @@
 
 #define SENSOR_READING_PIN A0
 const int SENSOR_POWER_PIN = 2;
-const float DRY_READING = 545.0;  // 600 if connected directly to the 5V power pin
-const float SCALE_FACTOR = -2.4;  // 1.6 if connected directly to the 5V power pin
+const float DRY_READING = 545.0,  // 600 if connected directly to the 5V power pin
+            SATURATED_READING = 305.0,
+            SCALE_FACTOR = (SATURATED_READING - DRY_READING)/100.0;  // -1.6 if connected directly to the 5V power pin
 
 void sensor_setup();
 float sensor_value();
